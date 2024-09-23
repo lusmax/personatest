0fae2cb9c90f53309e2c1bdb1eea463dd4bdc534
lusmax <lusmax0128@gmail.com>
2024-09-23 14:10:40 +0200

    create calcularIMC method in Persona class

--- personaExamen/src/main/java/paq/Persona.java
+++ personaExamen/src/main/java/paq/Persona.java
@@ -16,14 +16,14 @@ public class Persona {
 
     //atributos por defecto
     public static final String DEFAULT_NOMBRE = "";
-    public static final byte DEFAULT_EDAD = 0;    
+    public static final byte DEFAULT_EDAD = 0;
     public static final char DEFAULT_SEXO = 'H';
     public static final float DEFAULT_PESO = 0;
     public static final float DEFAULT_ALTURA = 0;
-    
+
     //constructores
-    public Persona(){
-        
+    public Persona() {
+
         this.nombre = Persona.DEFAULT_NOMBRE;
         this.edad = Persona.DEFAULT_EDAD;
         this.sexo = Persona.DEFAULT_SEXO;
@@ -102,5 +102,19 @@ public class Persona {
     @Override
     public String toString() {
         return "Persona{" + "nombre=" + nombre + ", edad=" + edad + ", Dni=" + Dni + ", sexo=" + sexo + ", peso=" + peso + ", altura=" + altura + '}';
-    }   
+    }
+
+    public int calcularIMC() {
+        int result;
+        double imc = this.peso / Math.pow(this.altura, 2);
+        if (imc < 18.5) {
+            result = -1;
+        } else if (imc > 24.9) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        return result;
+
+    }
 }
